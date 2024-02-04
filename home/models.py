d58d6c8dbf6301d6f42e1a27ac5f4511941d5c44
AppSeed Generated Projects <133563814+appseed-projects4@users.noreply.github.com>
2024-02-04 10:12:09 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,58 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+    name = models.CharField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    bio = models.CharField(max_length=255, null=True, blank=True)
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Undefined(models.Model):
+
+    #__Undefined_FIELDS__
+
+    #__Undefined_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Undefined")
+        verbose_name_plural = _("Undefined")
+
+
+class Employee(models.Model):
+
+    #__Employee_FIELDS__
+    employeeid = models.CharField(max_length=255, null=True, blank=True)
+    firstname = models.CharField(max_length=255, null=True, blank=True)
+    lastname = models.CharField(max_length=255, null=True, blank=True)
+    mobileno = models.CharField(max_length=255, null=True, blank=True)
+
+    #__Employee_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Employee")
+        verbose_name_plural = _("Employee")
+
+
+
+#__MODELS__END
